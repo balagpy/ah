354e1ddb94b6ff7b51cef1e28239bf0a8f66bebd
balagpy <balagpy@gmail.com>
2021-06-26 11:34:26 +0530

    Update twemoji.rb

--- config/initializers/twemoji.rb
+++ config/initializers/twemoji.rb
@@ -3,7 +3,7 @@
 require "twemoji/svg"
 
 Twemoji.configure do |config|
-  config.asset_root = ENV["TWEMOJI_ASSET_ROOT"] || "https://twemoji.ruby-china.com/2"
+  config.asset_root = ENV["TWEMOJI_ASSET_ROOT"] || "https://twitter.github.io/twemoji/2"
   config.file_ext = "svg"
   config.class_name = "twemoji"
 end
